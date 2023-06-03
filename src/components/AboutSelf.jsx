095e891f7e37045851903f6a32c0ad22d81f1d28
rsunar <tech.channel.002@gmail.com>
2023-06-03 09:39:56 +0530

    fixing loream epsom text

--- src/components/AboutSelf.jsx
+++ src/components/AboutSelf.jsx
@@ -1,40 +1,36 @@
-
-import classes from './AboutSelf.module.css';
-import SelfImage from '../assets/bamya2ndLot.jpg';
+import classes from "./AboutSelf.module.css";
+import SelfImage from "../assets/bamya2ndLot.jpg";
 const AboutSelf = () => {
-    const Info = [
-        {
-            image: SelfImage,
-        }
-    ]
+  const Info = [
+    {
+      image: SelfImage,
+    },
+  ];
   return (
     <>
-        <div className={classes.text}>
-          <h1>
-            What About Me ?!
-          </h1>
-          <hr className={classes.horLine}/>
-        </div>
-        <div className={classes.wrapper}>
+      <div className={classes.text}>
+        <h1>What About Me ?!</h1>
+        <hr className={classes.horLine} />
+      </div>
+      <div className={classes.wrapper}>
         {Info.map((info) => {
           return (
-            
-              <div className={classes.languageContainer}>
-                  <img src={info.image} alt="" className={classes.items} />
-               
-              </div>
-            
+            <div className={classes.languageContainer}>
+              <img src={info.image} alt="" className={classes.items} />
+            </div>
           );
         })}
       </div>
       <div className={classes.introText}>
         <h1 className={classes.nameText}>
-            <i>"ROSHAN DALAMI"</i>
+          <i>"ROSHAN DALAMI"</i>
         </h1>
-        <p className={classes.infoText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum iure blanditiis illum iste dolores dicta culpa qui debitis repellat provident sapiente cumque nostrum cum, itaque nihil modi? Placeat, minima vero.</p>
+        <p className={classes.infoText}>
+        Tech Enthusiast. Just Completed B.E Computer Science and Engineering. Passionate Web Developer. Currently exploring different technologies. Learning How to built world a better place with Code 
+        </p>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default AboutSelf
+export default AboutSelf;
